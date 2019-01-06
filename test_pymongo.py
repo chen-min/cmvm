fc0788589b56c3d2166d3882fd9c812f20acc93c
weibo-5146192729 <matthiaschen@126.com>
2019-01-06 08:37:30 +0800

    add

--- test_pymongo.py
+++ test_pymongo.py
@@ -11,9 +11,10 @@ class TestMongo(object):
   def add_one(self):
     ''''add data'''
     post = {
-      'title': '新的标题',  
-      'content': '博客内容....',
-      'created_at': datetime.now()
+      'title': 'test03',  
+      'content': 'test03....',
+      'created_at': datetime.now(),
+      'x':23
     }
     return self.db.blog.posts.insert_one(post)
 
@@ -26,16 +27,30 @@ class TestMongo(object):
   def get_one_from_oid(self, oid):
     obj = ObjectId(oid)
     return self.db.blog.posts.find_one({'_id': obj})
+  
+  def update(self):
+    # return self.db.blog.posts.update_one({'x':23}, {'$inc':{'x':111}})
+    return self.db.blog.posts.update_many({},{'$inc':{'x':10}})
+
+  def delete(self):
+    # return self.db.blog.posts.delete_one({'x':1})
+    return self.db.blog.posts.delete_many({'x':1})
 
 def main():
   obj = TestMongo()
-  # rest = obj.get_one()
+  # rest = obj.add_one()
   # print(rest["_id"],'rest')
   # rest = obj.get_more()
   # for item in rest:
   #   print(item["_id"])
   # rest = obj.get_one_from_oid('5c308ce57f1d0910248d7235')
   # print(rest)
+  # rest = obj.update()
+  # print(rest,'rest')
+  # print(rest.matched_count)
+  # print(rest.modified_count)
+  rest = obj.delete()
+  print(rest.deleted_count)
 
 if __name__ == "__main__":
   main()
